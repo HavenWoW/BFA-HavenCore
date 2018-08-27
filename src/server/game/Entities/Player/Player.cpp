9015aac56d4bf9b427036e82834c290eaf123abf
Treeston <treeston.mmoc@gmail.com>
2018-08-27 19:08:17 +0200

    Core/Threat: Threat system adjustments:
    * Online states are now re-evaluated before victim update instead of continuously. Closes #22226. Tagging #21501.
    * Victim update now happens every 1s as opposed to every server tick unless current target goes away.
    * Suppressed threat is no longer re-established until the victim gains additional threat (by hitting the target, for instance).
    * Assistance threat is now split between non-controlled units threatened by target, as opposed to all units threatened by target.
    
    (cherry picked from commit 5cea572a9ad524c6f28ff8519bee61d1ff4357d0)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -2464,7 +2464,6 @@ void Player::SetGameMaster(bool on)
         if (Pet* pet = GetPet())
         {
             pet->SetFaction(getFaction());
-            pet->GetThreatManager().UpdateOnlineStates();
         }
 
         // restore FFA PvP Server state
