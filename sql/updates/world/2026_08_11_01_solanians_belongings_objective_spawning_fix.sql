9157aee13370f577287096eb12b3e7379100923f
CDBrodie <chris.brodie@gmail.com>
2026-08-11 13:13:04 -0600

    Fixed SQL
    This was not updating properly as the SQL is missing a few semicolons (;)
    
    Fixed and tested

--- sql/updates/world/2026_08_11_01_solanians_belongings_objective_spawning_fix.sql
+++ sql/updates/world/2026_08_11_01_solanians_belongings_objective_spawning_fix.sql
@@ -1,12 +1,14 @@
 -- Journal
-UPDATE gameobject 
-SET spawntimesecs = 60 
-WHERE guid = 20007
+UPDATE gameobject
+SET spawntimesecs = 60
+WHERE guid = 20007;
+
 -- Scroll
-UPDATE gameobject 
-SET spawntimesecs = 60 
-WHERE guid = 20008
+UPDATE gameobject
+SET spawntimesecs = 60
+WHERE guid = 20008;
+
 -- Orb
-UPDATE gameobject 
-SET spawntimesecs = 60 
-WHERE guid = 19565
+UPDATE gameobject
+SET spawntimesecs = 60
+WHERE guid = 19565;
