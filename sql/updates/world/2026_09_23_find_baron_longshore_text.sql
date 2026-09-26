e61b89c213b2c1d16cc6153021c425bb66d3373e
Hex <132233897+Hextv@users.noreply.github.com>
2026-09-26 11:48:51 +0300

    Fix: INSERT IGNORE in quest text updates

--- sql/updates/world/2026_09_23_find_baron_longshore_text.sql
+++ sql/updates/world/2026_09_23_find_baron_longshore_text.sql
@@ -2,4 +2,4 @@
 -- https://www.wowhead.com/quest=14045/find-baron-longshore
 -- https://www.youtube.com/watch?v=mAlWXXbRNPU
 
-INSERT INTO quest_offer_reward (ID, Emote1, Emote2, Emote3, Emote4, EmoteDelay1, EmoteDelay2, EmoteDelay3, EmoteDelay4, RewardText, VerifiedBuild) VALUES (14045, 0, 0, 0, 0, 0, 0, 0, 0, "Hah-har! So ol/' Gazzy ain\'t given up on his goods yet, has he? Well, looks like that ol/' floppy-eared goldgrubbler jes/' gave me somethin/' ta bargain with.", 20574);
+INSERT IGNORE INTO quest_offer_reward (ID, Emote1, Emote2, Emote3, Emote4, EmoteDelay1, EmoteDelay2, EmoteDelay3, EmoteDelay4, RewardText, VerifiedBuild) VALUES (14045, 0, 0, 0, 0, 0, 0, 0, 0, "Hah-har! So ol/' Gazzy ain\'t given up on his goods yet, has he? Well, looks like that ol/' floppy-eared goldgrubbler jes/' gave me somethin/' ta bargain with.", 20574);
